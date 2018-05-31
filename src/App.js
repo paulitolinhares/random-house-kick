3d2a0a73155d8a983ddeaac8a56ea99b11678cb3
Paulo Neto <pauloneto@Paulos-MacBook-Pro.local>
2018-05-31 17:22:38 +0200

    adds router and page components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Joke from './components/pages/Joke';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Home}/>
+      <Route path="/joke/:category" component={Joke}/>
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/components/pages/Joke.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Joke = () => (
+    <h1>Joke page</h1>
+);
+
+export default Joke;
