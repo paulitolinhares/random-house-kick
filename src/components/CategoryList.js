4ae45ea8c4fb7fbd3523b85f8ced17d5dcfc069c
Paulo Neto <pauloneto@Paulos-MacBook-Pro.local>
2018-05-31 18:52:58 +0200

    refactoring category list to make it look like a list with images

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -5,15 +5,18 @@ import { capitalize } from '../lib/string';
 
 const CategoryList = ({ categories }) => {
     return (
-        <div className="CategoryList">
+        <ul className="CategoryList">
             { 
                 categories.map(category =>
-                    <Link key={category} to={`/jokes/${category}`} className="tag is-link is-large">
-                        {category}
-                    </Link>
+                    <li key={category}>
+                        <Link key={category} to={`/jokes/${category}`}>
+                            <img src="https://assets.chucknorris.host/img/avatar/chuck-norris.png" alt=""/>
+                            {category}
+                        </Link>
+                    </li>
                 )
             }
-        </div>
+        </ul>
     );
 };
 
