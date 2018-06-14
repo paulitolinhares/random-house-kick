2fa37cd210a59127b2a68d11d87f3a8d05749029
Paulo Linhares <paulitolinhares@gmail.com>
2018-06-14 11:27:54 +0200

    refactoring Joke page to graphql (TODO: Done state)

--- src/components/containers/Joke/index.js
+++ src/components/containers/Joke/index.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { fetchJoke, jokeDone } from 'actions';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 import JokeLoading from 'components/presentationals/JokeLoading';
 import JokeDone from 'components/presentationals/JokeDone';
 import JokeContent from 'components/presentationals/JokeContent';
@@ -10,50 +10,49 @@ import PageShell from 'components/hocs/PageShell';
 
 class Joke extends PureComponent {
 
-  componentWillMount() {
-    this.loadJoke();
-  }
-
   componentDidUpdate(prevProps) {
-    if (
-      this.props.jokeStatus !== prevProps.jokeStatus &&
-      this.props.jokeStatus === 'loaded'
-    ) {
-      this.timeout = window.setTimeout(() => {
-        this.props.jokeDone();
-      }, 2150); // 2150 is the time for the gif to complete
-    }
-  }
-
-  loadJoke = () => {
-    const { match: { params: { category } } } = this.props;
-    this.props.fetchJoke(category);
+    // if (
+    //   this.props.jokeStatus !== prevProps.jokeStatus &&
+    //   this.props.jokeStatus === 'loaded'
+    // ) {
+    //   this.timeout = window.setTimeout(() => {
+    //     this.props.jokeDone();
+    //   }, 2150); // 2150 is the time for the gif to complete
+    // }
   }
 
   render() {
-    const { jokeStatus, joke } = this.props;
+    const { joke, loading, error } = this.props.jokeQuery;
+    console.log(this.props);
     return (
       <div className="Joke">
-      {
-        {
-          loading: <JokeLoading />,
-          loaded: <JokeDone />,
-          done: <JokeContent joke={joke} newJoke={this.loadJoke} back={this.props.history.goBack} />,
-          failed: <Error newJoke={this.loadJoke} back={this.props.history.goBack} />
-        }[jokeStatus]
-      }
+        {joke && <JokeContent joke={joke} newJoke={this.loadJoke} back={this.props.history.goBack} />}
+        {loading && <JokeLoading />}
+        {error && <Error newJoke={this.loadJoke} back={this.props.history.goBack} />}
       </div>
     );
   }
 }
 
 Joke.propTypes = {
-  joke: PropTypes.string,
-  jokeStatus: PropTypes.string.isRequired,
-  fetchJoke: PropTypes.func.isRequired,
-  jokeDone: PropTypes.func.isRequired
+  jokeQuery: PropTypes.shape({
+    loading: PropTypes.bool,
+    error: PropTypes.bool,
+    joke: PropTypes.string,
+  })
 };
 
-const mapStateToProps = ({ joke, jokeStatus }) => ({ joke, jokeStatus });
+const JOKE_QUERY = gql`
+query JokeQuery($category: String) {
+  joke(category: $category)
+}
+`
 
-export default connect(mapStateToProps, { fetchJoke, jokeDone })(PageShell(Joke));
+export default graphql(JOKE_QUERY, {
+  name: 'jokeQuery',
+  options: ownProps => ({
+    variables: {
+      category: ownProps.match.params.category
+    }
+  })
+})(PageShell(Joke));
